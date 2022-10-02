5f898ecda6065f31633b1ef21eee736cf0b79e1c
mbitzos <m.bitzos@gmail.com>
2022-10-01 20:25:04 -0400

    Update sidebar.vue

--- src/views/devblog/components/sidebar.vue
+++ src/views/devblog/components/sidebar.vue
@@ -5,7 +5,10 @@
     </div>
     <ul class="articles">
       <li v-for="article in articles" :key="article.key">
-        <router-link class="article" :to="`/devblog/${article.key}`">{{
+        <div v-if="isSelected(article)">
+          {{ article.title }}
+        </div>
+        <router-link v-else :to="`/devblog/${article.key}`">{{
           article.title
         }}</router-link>
       </li>
@@ -15,11 +18,18 @@
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 import Posts from "../posts/index";
+import { Post } from "@/types";
 export default defineComponent({
   data() {
     return {
       articles: Posts
     };
+  },
+  methods: {
+    isSelected(post: Post) {
+      console.log(this.$route.name);
+      return post.key == this.$route.name;
+    }
   }
 });
 </script>
@@ -35,9 +45,6 @@ export default defineComponent({
   .articles {
     margin-left: 20px;
     list-style: circle;
-    .article {
-      color: $old-link;
-    }
   }
 }
 </style>
