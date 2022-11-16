1f7b63c8c1961c1208cc8734fc3bb4206f08ffb8
mbitzos <m.bitzos@gmail.com>
2022-11-15 23:28:37 -0500

    add font awesome + copy button to code

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import "highlight.js/styles/github-dark-dimmed.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 require("@/assets/main.scss");
-
+library.add(fas, far);
 createApp(App)
   .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
--- src/views/devblog/components/code.vue
+++ src/views/devblog/components/code.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="code-block">
+  <div class="code-block" ref="mySlot">
+    <FontAwesomeIcon
+      icon="fa-regular fa-copy"
+      class="copy-icon"
+      @click="copy"
+    />
     <slot></slot>
   </div>
 </template>
 <script lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { defineComponent } from "vue";
 export default defineComponent({
-  components: {}
+  components: { FontAwesomeIcon },
+  data() {
+    return {
+      code: ""
+    };
+  },
+  methods: {
+    async copy() {
+      await navigator.clipboard.writeText(this.code);
+    }
+  },
+  mounted() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    this.code = (this.$refs.mySlot as any).textContent;
+  }
 });
 </script>
 <style lang="scss">
 .code-block {
+  position: relative;
+
+  .copy-icon {
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: white;
+    transition: 0.2s;
+
+    &:hover {
+      color: $pink;
+    }
+  }
   pre {
     padding: 0;
     margin-top: 1em;
